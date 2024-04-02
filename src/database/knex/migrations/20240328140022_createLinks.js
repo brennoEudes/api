7876a3ba168992c42f56f398ee656099d38c8e87
“Brenno <dev.brennoeudes@gmail.com>
2024-04-01 21:07:41 -0300

    .onDelete Cascade correction

--- src/database/knex/migrations/20240328140022_createLinks.js
+++ src/database/knex/migrations/20240328140022_createLinks.js
@@ -2,7 +2,11 @@ exports.up = (knex) =>
   knex.schema.createTable("links", (table) => {
     // campos da tabela:
     table.increments("id");
-    table.integer("note_id").references("id").inTable("notes");
+    table
+      .integer("note_id")
+      .references("id")
+      .inTable("notes")
+      .onDelete("CASCADE");
     table.text("url").notNullable(); // mét q não aceita nulo
 
     table.timestamp("created_at").default(knex.fn.now());
