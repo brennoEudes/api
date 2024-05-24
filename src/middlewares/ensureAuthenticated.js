1e471a640d1f8cc6f0e57b316291c00880e18bae
“Brenno <dev.brennoeudes@gmail.com>
2024-05-24 18:51:17 -0300

    authentication middleware structuring

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,32 @@
+const { verify } = require("jsonwebtoken"); // fcn disponível no jwt
+const AppError = require("../utils/AppError");
+const authConfig = require("../configs/auth");
+const auth = require("../configs/auth");
+
+// middleware recebe o "next" p/ chamar a próx. fcn destino da REQ:
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.headers.authorization; // acessando o cabeçalho da REQ
+
+  // se ñ tiver token:
+  if (!authHeader) {
+    throw new AppError("JWT token não informado!", 401);
+  }
+
+  // se existir:
+  const [, token] = authHeader.split(" "); // split quebra o texto no caracter espaço e assim capturamos a 2º posição no array (token).
+
+  // verifica se token é válido:
+  try {
+    const { sub: user_id } = verify(token, authConfig(jwt.secret));
+
+    request.user = {
+      id: Number(user_id), // guardando o id como número no DB
+    };
+
+    return next(); // chama a fcn destino
+  } catch {
+    throw new AppError("JWT inválido!", 401);
+  }
+}
+
+module.exports = ensureAuthenticated;
